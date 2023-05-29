87c22fe99c840bfee7a167c77fe2c06b84e1fe44
Frank Tropschuh <gunther@idoru.ch>
2023-05-29 17:37:44 +0200

    add modules

---
+++ kubectl_query/config.py
@@ -0,0 +1,99 @@
+import logging
+import sys
+import yaml
+from jsonpath_ng import parse
+from kubernetes import dynamic, config as kubeconfig
+from kubernetes.client import api_client
+from colors import color
+
+logger = logging.getLogger(__name__)
+
+
+class Config:
+    """
+    Represents the config file and does sanity checking of the input
+    """
+
+    def __init__(self, configfile):
+        """
+        Parse the configuration file and compile the JSON paths needed
+        to select fields later on
+        """
+
+        self._client = dynamic.DynamicClient(api_client.ApiClient(configuration=kubeconfig.load_kube_config()))
+
+        self.configfile = configfile
+        logger.debug(f"Reading {configfile}")
+        with open(configfile, "r", encoding="utf-8") as stream:
+            try:
+                self.config = yaml.safe_load(stream)
+
+                # process tables
+                for table, prop in self.config.get("tables", {}).items():
+                    logger.debug(f"  Loading config for table {table}")
+                    # for fields we need to compile the path for l
+                    for field, path in prop.get("fields", {}).items():
+                        prop["fields"][field] = parse(path)
+
+                # process and queries
+                for query, prop in self.config.get("queries", {}).items():
+                    logger.debug(f"  Loading config for query {query}")
+
+            except yaml.YAMLError as exception:
+                logger.critical(exception)
+                sys.exit(1)
+
+    def get_available_queries(self):
+        """
+        Put together a message with all the config data
+        """
+
+        messages = [
+            "",
+            "Query multiple cluster resources and join them together as tables",
+        ]
+
+        messages.extend(["", "Tables:", ""])
+        for table, prop in self.config.get("tables", {}).items():
+            messages.append(f"  {color(table, fg='yellow')}")
+            if prop.get("note"):
+                messages.append(f"    {prop['note']}")
+
+        messages.extend(["", "Queries:", ""])
+        for query, prop in self.config.get("queries", {}).items():
+            messages.append(f"  {color(query, fg='yellow')}")
+            if prop.get("note"):
+                messages.append(f"    {prop['note']}")
+        messages.append("")
+
+        return messages
+
+    def check_queries(self, queries):
+        """
+        Make sure that all requested queries are defined, otherwise show
+        the user what we have
+        """
+
+        available = list(self.config.get("queries", {}).keys()) + list(self.config.get("tables", {}).keys())
+
+        if all(query in available for query in queries):
+            return queries
+
+        errors = list(filter(lambda query: query not in available, queries))
+        logger.error(f"{errors} are neither tables nor queries")
+        return []
+
+    @property
+    def client(self):
+        """Just the API client"""
+        return self._client
+
+    @property
+    def tables(self):
+        """All available tables"""
+        return self.config["tables"]
+
+    @property
+    def queries(self):
+        """All available queries"""
+        return self.config["queries"]
---
+++ kubectl_query/table.py
@@ -0,0 +1,52 @@
+import itertools
+import pandas as pd
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class Table(pd.DataFrame):
+    """
+    Akin to running a
+      kubectl get <kind> -A -o json
+    and apply the various precompiled paths across the resulting items
+
+    If path searches return multiple items, unroll the list to produce
+    multiple records
+    """
+
+    def __init__(self, client, api_version, kind, fields, **_):
+        """
+        Table is really just a fancy constructor for a DataFrame that stores
+        the result of an API call in table format... the magic lies within
+        the fact that json path queries that result in multiple matches
+        trigger multiple rows to be created, so we can treat each data set
+        independently when joining other tables
+        """
+
+        def product_dict(**kwargs):
+            """
+            Gem found at
+            https://stackoverflow.com/questions/5228158
+            """
+            keys = kwargs.keys()
+            for instance in itertools.product(*kwargs.values()):
+                yield dict(zip(keys, instance))
+
+        # get all resources, all namespaces
+        resource = client.resources.get(api_version=api_version, kind=kind)
+        items = []
+        for entry in resource.get().items:
+            # extract fields by going through all paths requested
+            item = {}
+            for field, path in fields.items():
+                item[field] = [str(match.value) for match in path.find(entry)]
+
+            # expand the result and add to table
+            items.extend(product_dict(**item))
+
+        logger.debug(f"  Loaded {len(items)} {kind}")
+
+        # throw the resulting items into a DataFrame
+        super().__init__(items)
