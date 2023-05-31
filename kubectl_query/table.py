4b603f6aec522600c2bed4cce6a09e001ae27f18
Frank Tropschuh <gunther@idoru.ch>
2023-05-31 21:54:04 +0200

    issue #9 - do not expand fields that are linked into Cartesian product of overall records

--- kubectl_query/table.py
+++ kubectl_query/table.py
@@ -16,7 +16,7 @@ class Table(pd.DataFrame):
     multiple records
     """
 
-    def __init__(self, client, table, api_version, kind, fields, **_):
+    def __init__(self, client, table, api_version, kind, fields, **kwargs):
         """
         Table is really just a fancy constructor for a DataFrame that stores
         the result of an API call in table format... the magic lies within
@@ -27,21 +27,38 @@ class Table(pd.DataFrame):
 
         def product_dict(**kwargs):
             """
-            Gem found at
-            https://stackoverflow.com/questions/5228158
+            Unroll the combinations, may even be within a column if
+            subqueries were used
             """
             keys = kwargs.keys()
             for instance in itertools.product(*kwargs.values()):
-                yield dict(zip(keys, instance))
+                values = {}
+                for field, value in dict(zip(keys, instance)).items():
+                    if isinstance(value, dict):
+                        for subfield, subvalue in value.items():
+                            values[subfield] = str(subvalue)
+                    else:
+                        values[field] = str(value)
+                yield values
 
         # get all resources, all namespaces
         resource = client.resources.get(api_version=api_version, kind=kind)
         items = []
+
         for entry in resource.get().items:
             # extract fields by going through all paths requested
             item = {}
             for field, path in fields.items():
-                item[field] = [str(match.value) for match in path.find(entry)]
+                if isinstance(path, dict):
+                    subitem = {}
+                    for subfield, subpath in path.items():
+                        subitem[subfield] = [match.value for match in subpath.find(entry)]
+
+                    # dict of lists to list of dicts
+                    item[field] = [dict(zip(subitem, i)) for i in zip(*subitem.values())]
+
+                else:
+                    item[field] = [str(match.value) for match in path.find(entry)]
 
             # expand the result and add to table
             items.extend(product_dict(**item))
